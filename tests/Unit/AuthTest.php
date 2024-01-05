39a0750c56f12949728081ef64901f2d0bab07f3
Arbi Syarifudin <arbisyarifudin@gmail.com>
2024-01-05 09:17:03 +0700

    ci: Update testing workflow and database configuration for unit tests

--- tests/Unit/AuthTest.php
+++ tests/Unit/AuthTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    use DatabaseMigrations;
     use DatabaseTransactions;
 
     /** @test */
